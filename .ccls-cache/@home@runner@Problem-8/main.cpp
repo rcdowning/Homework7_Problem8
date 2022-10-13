82ff5b7e57b7b96397d51303996e78f86304e6d7
rcdowning <111823343+rcdowning@users.noreply.github.com>
2022-10-13 00:50:47 +0000

    setting up problem

---
+++ .ccls-cache/@home@runner@Problem-8/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+  // how many times a num is repeated and how many distinct values
+
+  // create two arrays to keep track of both of these things
+
+  // for loop for reading in array
+
+  // two parallel loops: one for freqvalues and num of occurances
+    for
+      if
+        [loc] = freqvals numoccurs = +1;
+}
